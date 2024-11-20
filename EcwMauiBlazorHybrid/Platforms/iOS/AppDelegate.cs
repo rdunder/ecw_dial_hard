5027e4c209033f895e40c8f26a354015e379021d
rdunder <r.dunder@gmail.com>
2024-11-20 14:47:30 +0100

    Init

---
+++ EcwMauiBlazorHybrid/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace EcwMauiBlazorHybrid
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
